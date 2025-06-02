ab0f2a223d27d0e5a914c20ccf0e5b5f242454b7
Raquel Queiroz <113996452+slowrach@users.noreply.github.com>
2025-06-02 11:00:27 -0300

    criação de inputs controlados

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
 import "./App.css"
+import { Controller, useForm, type ControllerProps } from "react-hook-form"
+
+type FormData = {
+   name: string
+   lastName: string
+   email: string
+   mentor: string
+   feedback: string
+   date: string
+   start: string
+   end: string
+   check: boolean
+}
 
 export function App() {
+   const { control, handleSubmit } = useForm<FormData>({ defaultValues: {
+      name: "",
+      lastName: "",
+      email: "",
+      mentor: "",
+      feedback: "",
+      date: "",
+      start: "",
+      end: "",
+      check: false
+   } })
+
+   function submit(data: FormData){
+      console.log(data)
+   }
+
   return (
     <div className="container">
       <header>
@@ -13,22 +42,25 @@ export function App() {
          <h2>Informações pessoais</h2>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit(submit)}>
          <fieldset>
             <div className="full-name">
                <div>
                   <h3>Nome</h3>
-                  <input type="text" required></input>
+
+                  <Controller control={control} name="name" render={({ field }) => (<input type="text" {...field}/>)} />
                </div>
                <div>
                   <h3>Sobrenome</h3>
-                  <input type="text" required></input>
+                  
+                  <Controller control={control} name="lastName" render={({ field }) => (<input type="text" {...field}/>)} />
                </div>
             </div>
 
             <div className="email-data">
                <h3>E-mail <span>(Digite um email válido)</span></h3>
-               <input type="email" required></input>
+
+               <Controller control={control} name="email" render={({ field }) => (<input type="email" {...field}/>)} />
             </div>
          </fieldset>
             
@@ -38,24 +70,27 @@ export function App() {
                <div className="mentor-name">
                   <h3>Selecione quem foi o seu mentor</h3>
 
-                  <select>
-                     <option value="guitarist1">Mark Knopfler</option>
-                     <option value="guitarist2">Eric Clapton</option>
-                     <option value="guitarist3">Gary Moore</option>
-                     <option value="guitarist4">David Gilmour</option>
-                     <option value="guitarist5">Jeff Beck</option>
-                     <option value="guitarist6">Stevie Ray Vaughan</option>
-                  </select>
+                  <Controller control={control} name="mentor" render={({ field }) => (
+                     <select {...field}>
+                        <option value=" Mark Knopfler">Mark Knopfler</option>
+                        <option value="Eric Clapton">Eric Clapton</option>
+                        <option value="Gary Moore">Gary Moore</option>
+                        <option value="David Gilmour">David Gilmour</option>
+                        <option value="Jeff Beck">Jeff Beck</option>
+                        <option value="Stevie Ray Vaughan">Stevie Ray Vaughan</option>
+                     </select>
+                  )} />
                </div>
 
                <div className="mentor-feedback">
                   <h3>Feedback sobre o mentor</h3>
 
-                  <textarea></textarea>
+                  <Controller control={control} name="feedback" render={({ field }) => (<textarea {...field}/>)} />
                </div>
 
                <div className="checkbox">
-                  <input type="checkbox" ></input>
+                  <Controller control={control} name="check" render={({ field }) => (<input type="checkbox" {...field}  value=""/>
+  )} />
 
                   <h3>Todas as minhas dúvidas foram esclarecidas durante a mentoria</h3>
                </div>
@@ -69,23 +104,26 @@ export function App() {
                <div className="date">
                   <h3>Data <span>(DD/MM/AAAA)</span></h3>
 
-                  <input type="date"></input>
+                  <Controller control={control} name="date" render={({ field }) => (<input type="date" {...field}/>)} />
                </div>
 
                <div className="hour">
                   <div>
                      <h3>Das</h3>
-                     <input type="time"></input>
+
+                     <Controller control={control} name="start"render={({ field }) => (<input type="time" {...field}/>)} />
                   </div>
+
                   <div>
                      <h3>Até</h3>
-                     <input type="time"></input>
+
+                     <Controller control={control} name="end" render={({ field }) => (<input type="time" {...field}/>)} />
                   </div>
                </div>
             </div>
          </fieldset>
 
-         <button className="button" type="button">
+         <button className="button" type="submit">
             Salvar
          </button>
       </form>
